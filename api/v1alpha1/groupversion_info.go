bf783844dfc769a496e68dc3917773290ba1d6a8
Ismail KABOUBI <ikaboubi@gmail.com>
2022-03-07 18:55:09 +0100

    updating release store crd

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the release-controller.io v1alpha1 API group
 //+kubebuilder:object:generate=true
-//+groupName=release-controller.io.release-controller.io
+//+groupName=release-controller.io
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "release-controller.io.release-controller.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "release-controller.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
